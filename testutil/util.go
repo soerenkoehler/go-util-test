b6b47c8380cf2c467e71b4fcee391306805bd33a
Sören Köhler <soerenkoehler@users.noreply.github.com>
2021-11-04 21:36:05 +0100

    test suites

--- testutil/util.go
+++ testutil/util.go
@@ -2,6 +2,27 @@ package testutil
 
 import "testing"
 
+type Suite map[string]func(*testing.T)
+
+func RunSuite(
+	t *testing.T,
+	setup func(*testing.T),
+	teardown func(*testing.T),
+	suite Suite) {
+
+	for testName, testFunc := range suite {
+		if setup != nil {
+			setup(t)
+		}
+
+		t.Run(testName, testFunc)
+
+		if teardown != nil {
+			teardown(t)
+		}
+	}
+}
+
 func ShouldPanic(t *testing.T, f func()) {
 	defer func() { recover() }()
 	f()
